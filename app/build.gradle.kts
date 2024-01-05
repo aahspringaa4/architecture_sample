c2f73a2b4af1bc0dc295a4343846f7a7b0ffa932
yoohyunmyeong <aahspringaa4@naver.com>
2024-01-05 16:54:51 +0900

    build: add ui module dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,5 +57,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    implementation(project(mapOf("path" to ":ui")))
 }
