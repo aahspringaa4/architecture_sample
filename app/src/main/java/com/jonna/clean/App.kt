e05643946328436e4cd07b0b7b7288b706d743aa
yoohyunmyeong <aahspringaa4@naver.com>
2024-01-05 16:55:01 +0900

    feat: add first application

--- app/src/main/java/com/jonna/clean/App.kt
+++ app/src/main/java/com/jonna/clean/App.kt
@@ -1,8 +1,18 @@
 package com.jonna.clean
 
 import android.app.Application
+import android.content.Intent
+import com.jonna.ui.MainActivity
 
 class App: Application() {
 
-    override fun onCreate() { super.onCreate() }
+    /*
+    This class will have information or permission about device
+    */
+    override fun onCreate() {
+        super.onCreate()
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
 }
