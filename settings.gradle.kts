abdfa4404f1bf38e7bae054abc2bc91804376906
yoohyunmyeong <aahspringaa4@naver.com>
2024-01-05 12:34:14 +0900

    feat: add jonna clean architecture's module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,14 @@ dependencyResolutionManagement {
 
 rootProject.name = "architecture_sample"
 include(":app")
- 
+include(":ui")
+include(":presentation")
+include(":network")
+include(":infra")
+include(":storage")
+include(":device")
+include(":db")
+include(":api")
+include(":data")
+include(":domain")
+include(":model")
